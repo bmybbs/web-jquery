2ef3ff64cccdd6e2f112765c014cb1902570e3dd
IronBlood <aplshenyang@gmail.com>
2013-12-05 20:48:47 +0800

    更新登录函数

--- js/bmy.js
+++ js/bmy.js
@@ -102,3 +102,36 @@ function load_personal_status() {
 		});
 	}
 }
+
+function bind_login_button(callback) {
+	$('button#btn-login').click(function() {
+		var userid   = $('input#username').val();
+		var passwd   = $('input#password').val();
+		var is_rmbme = $('input#chk_remember').is(':checked');
+
+		var url_login = 'api/user/login?userid='+userid+'&passwd='+passwd+'&appkey=newweb';
+
+		$.ajax({
+			type: "GET",
+			url: url_login,
+			dataType: 'json',
+			success: function(data) {
+				if(data.errcode != 0) {
+					alert(data.errcode);
+				} else {
+					localStorage.userid = data.userid;
+					localStorage.sessid = data.sessid;
+					localStorage.token  = data.token;
+					localStorage.is_rmbme = is_rmbme;
+					if(callback && typeof(callback)=="function") {
+						callback();
+					}
+				}
+			},
+			error: function(xhr, ajaxOptions, thrownError) {
+				alert('oh no!' + xhr.responseText);
+				alert(thrownError);
+			}
+		});
+	});
+}
