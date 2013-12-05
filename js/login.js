1c2c00187071a4bb1e564aeb12860d1e09c6ef5c
IronBlood <aplshenyang@gmail.com>
2013-12-05 14:14:50 +0800

    将登陆框按钮判断的代码转入 index.html 页面中

--- js/login.js
+++ js/login.js
@@ -26,32 +26,3 @@ $('button#btn-login').click(function() {
 		}
 	});
 });
-
-$(document).ready(function (){
-	var userid = localStorage.userid;
-	var sessid = localStorage.sessid;
-	var is_local_rmbme = localStorage.is_rmbme;
-
-	if(typeof(is_local_rmbme) == 'undefined' || typeof(userid) == 'undefined' || typeof(sessid) == 'undefined') {
-		$('div#login-form').removeClass('hidden');
-		return;
-	}
-
-	if(is_local_rmbme==null || userid==null || sessid==null) {
-		$('div#login-form').removeClass('hidden');
-		return;
-	}
-
-	var url_checksession = 'api/user/checksession?userid='+userid+'&sessid='+sessid+'&appkey=newweb';
-
-	$.getJSON(url_checksession, function(data) {
-		if(data.errcode == 0) {
-			$('div#direct-login').removeClass('hidden');
-		} else {
-			$('div#login-form').removeClass('hidden');
-		}
-	})
-	.fail(function() {
-		$('div#login-form').removeClass('hidden');
-	});
-});
